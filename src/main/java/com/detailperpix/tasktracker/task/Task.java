c835e5f1a90826ee097b9b9250f63dcd93b1d12c
Wirmantono <wirmantonochong@gmail.com>
2022-09-20 00:46:23 +0700

    Change labels property into a single label, add labelId property

--- src/main/java/com/detailperpix/tasktracker/task/Task.java
+++ src/main/java/com/detailperpix/tasktracker/task/Task.java
@@ -3,17 +3,44 @@ package com.detailperpix.tasktracker.task;
 import java.util.Date;
 
 public class Task {
-    String[] labels;
+    int labelId;
+    String label;
     String title;
     String desc;
     Date startTime;
     Date endTime;
 
-    public Task(String[] labels, String title, String desc) {
-        this.labels = labels;
+    public Task(int labelId, String label, String title, String desc, long startTime, long endTime) {
+        this.labelId = labelId;
+        this.label = label;
         this.title = title;
         this.desc = desc;
-        this.startTime = new Date();
+        this.startTime = new Date(startTime);
+        this.endTime = new Date(endTime);
+    }
+
+    public long getStartTimeInEpochMilliseconds() {
+        return this.startTime.getTime();
+    }
+
+    public long getEndTimeInEpochMilliseconds() {
+        return this.endTime != null ? this.endTime.getTime() : -1;
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDesc() {
+        return this.desc;
+    }
+
+    public int getLabelId() {
+        return labelId;
     }
 
     public void finishTask() {
@@ -21,4 +48,19 @@ public class Task {
         this.endTime = new Date();
     }
 
+    @Override
+    public String toString() {
+        return String.format("Task: \n" +
+                "Title: %s \n" +
+                "Label: %s Id: %d\n" +
+                "Description: %s\n" +
+                "Start Time: %s\n" +
+                "End Time: %s",
+                getTitle(),
+                getLabel(),
+                getLabelId(),
+                getDesc(),
+                this.startTime,
+                this.endTime != null ? this.endTime : "NOT DONE" );
+    }
 }
